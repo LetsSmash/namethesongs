458ac55c1f8d119531a874893a9f03c1ce333913
LetsSmash <maxdinglinger02@gmail.com>
2024-03-18 12:58:44 +0100

    renamed the app once again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import React from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Guess The Songs",
+  title: "Name The Songs",
   description: "A Website to test your Knowledge on certain Albums.",
 };
 
@@ -20,7 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
           <div className="sm:mx-auto sm:w-full sm:max-w-md">
-            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Guess the Songs</h2>
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Name the Songs</h2>
             {children}
           </div>
         </div>
