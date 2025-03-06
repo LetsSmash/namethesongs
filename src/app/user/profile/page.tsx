ac93bfb53dccbf7418a97bb07910632daed08bd9
Max Dinglinger <md@tecno.de>
2025-03-06 13:46:12 +0100

    removed literal

--- src/app/user/profile/page.tsx
+++ src/app/user/profile/page.tsx
@@ -9,7 +9,7 @@ export default async function Page() {
         <div className="flex justify-center items-center">
             <FormBackground additionalClasses="w-full max-w-md">
                 {user?.hasImage && <Image src={user?.imageUrl} alt="Profile Picture" width={100} height={100}/>}
-                <h2 className="text-center text-3xl font-bold">{user?.username}'s Profile</h2>
+                <h2 className="text-center text-3xl font-bold">{user?.username}s Profile</h2>
                 <Divider />
             </FormBackground>
         </div>
